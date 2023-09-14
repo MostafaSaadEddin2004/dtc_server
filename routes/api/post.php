0597f5e7804fd8197f68b09bdbf63682b7cd3a70
= <=>
2023-09-14 15:30:38 +0300

    middleware

--- routes/api/post.php
+++ routes/api/post.php
@@ -4,8 +4,10 @@ use App\Http\Controllers\Api\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('post', PostController::class)->only(['index']);
-Route::post('/post/{post}/like', [PostController::class, 'like']);
-Route::post('/post/{post}/save', [PostController::class, 'save']);
 Route::middleware('auth:sanctum', 'userType:teacher')->group(function () {
     Route::apiResource('post', PostController::class)->except(['show', 'index']);
+});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/post/{post}/like', [PostController::class, 'like']);
+    Route::post('/post/{post}/save', [PostController::class, 'save']);
 });
