2df1a74184217e1d337ec3a55196f791934860d4
Loai Dev <loaidev64@gmail.com>
2023-09-17 14:35:27 +0300

    post liked by me

--- routes/api/post.php
+++ routes/api/post.php
@@ -3,11 +3,11 @@
 use App\Http\Controllers\Api\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('post', PostController::class)->only(['index']);
 Route::middleware('auth:sanctum', 'userType:teacher')->group(function () {
     Route::apiResource('post', PostController::class)->except(['show', 'index']);
 });
 Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('post', PostController::class)->only(['index']);
     Route::post('/post/{post}/like', [PostController::class, 'like']);
     Route::post('/post/{post}/save', [PostController::class, 'save']);
 });
